1fdf2f01729b5a7560b14df5baff38536773afc4
Dan Serban <dserban01@yahoo.com>
2016-04-15 19:28:26 +0300

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,7 @@
 echo $(hostname) > /opt/spark/conf/slaves
 
 ${KAFKA_HOME}/bin/zookeeper-server-start.sh -daemon ${KAFKA_HOME}/config/zookeeper.properties
+sleep 1
 ${KAFKA_HOME}/bin/kafka-server-start.sh -daemon ${KAFKA_HOME}/config/server.properties
 
 ${KAFKA_HOME}/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
