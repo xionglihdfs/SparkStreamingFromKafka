c5357795f8050eecf477b4a94a562aff907dee44
Dan Serban <dserban01@yahoo.com>
2016-04-15 18:33:15 +0300

    Add infinite loop in kafkaproducer.py

--- kafkaproducer.py
+++ kafkaproducer.py
@@ -1,9 +1,11 @@
 from kafka import KafkaProducer
+from time  import sleep
 
 producer = KafkaProducer()
 
-for i in range(1,10):
-    producer                            \
-    .send( 'test',  str(i), key='aaa' ) \
-    .get()
-
+while True:
+    for i in range(1,10):
+        producer                            \
+        .send( 'test',  str(i), key='aaa' ) \
+        .get()
+    sleep(20)
