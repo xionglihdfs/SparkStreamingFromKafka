59f618f3ba2871808986cdf321cb373f9a5e7359
Dan Serban <dserban01@yahoo.com>
2016-09-07 22:48:30 +0300

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -5,11 +5,11 @@ echo "Starting Kafka, please wait ..."
 ${KAFKA_HOME}/bin/zookeeper-server-start.sh -daemon ${KAFKA_HOME}/config/zookeeper.properties
 ${KAFKA_HOME}/bin/kafka-server-start.sh -daemon ${KAFKA_HOME}/config/server.properties
 
-echo "Idling while Kafka is fully operational, please wait ..."
+echo "Waiting for Kafka to become fully operational ..."
 
 sleep 5
 
-echo "Creating a topic, please wait ..."
+echo "Creating a topic ..."
 
 ${KAFKA_HOME}/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
 
@@ -17,11 +17,13 @@ JAR1=${HOME}/.ivy2/cache/com.yammer.metrics/metrics-core/jars/metrics-core-2.2.0
 JAR2=${HOME}/.ivy2/cache/org.apache.kafka/kafka_2.10/jars/kafka_2.10-0.8.2.1.jar
 JAR3=${HOME}/.ivy2/cache/org.apache.spark/spark-streaming-kafka_2.10/jars/spark-streaming-kafka_2.10-1.6.1.jar
 
-python /opt/kafkaproducer.py &
+/usr/bin/kafkaproducer.py &
 
-echo "Executing Spark Streaming application, please wait ..."
+echo "Waiting until enough messages have accumulated ..."
 
 sleep 20
+
+echo "Executing Spark Streaming application ..."
 
 spark-submit                     \
   --class="Streaming"            \
