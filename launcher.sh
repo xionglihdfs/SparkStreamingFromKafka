c24c8263ad949dc630a27e162b06e9f50f57d918
Dan Serban <dserban01@yahoo.com>
2016-09-07 23:00:30 +0300

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -14,8 +14,8 @@ echo "Creating a topic ..."
 ${KAFKA_HOME}/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
 
 JAR1=${HOME}/.ivy2/cache/com.yammer.metrics/metrics-core/jars/metrics-core-2.2.0.jar
-JAR2=${HOME}/.ivy2/cache/org.apache.kafka/kafka_2.10/jars/kafka_2.10-0.8.2.1.jar
-JAR3=${HOME}/.ivy2/cache/org.apache.spark/spark-streaming-kafka_2.10/jars/spark-streaming-kafka_2.10-1.6.1.jar
+JAR2=${HOME}/.ivy2/cache/org.apache.kafka/kafka_2.11/jars/kafka_2.11-0.8.2.1.jar
+JAR3=${HOME}/.ivy2/cache/org.apache.spark/spark-streaming-kafka_2.11/jars/spark-streaming-kafka_2.11-1.6.2.jar
 
 /usr/bin/kafkaproducer.py &
 
