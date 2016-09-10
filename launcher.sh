dbc5303077f676adceca85ceb20238f9fae395e5
Dan Serban <dserban01@yahoo.com>
2016-09-11 00:22:08 +0300

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 echo $(hostname) > /opt/spark/conf/slaves
 
-echo "Starting Kafka and Waiting for it to become fully operational, please wait ..."
+echo "Starting Kafka and waiting for it to become fully operational ..."
 
 ${KAFKA_HOME}/bin/zookeeper-server-start.sh -daemon ${KAFKA_HOME}/config/zookeeper.properties
 ${KAFKA_HOME}/bin/kafka-server-start.sh -daemon ${KAFKA_HOME}/config/server.properties
