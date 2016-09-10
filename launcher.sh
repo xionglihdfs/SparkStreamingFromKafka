d07bc02114ecde936ef8800f0105d479a136c66c
Dan Serban <dserban01@yahoo.com>
2016-09-10 23:53:01 +0300

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -16,6 +16,7 @@ ${KAFKA_HOME}/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replicat
 JAR1=${HOME}/.ivy2/cache/com.yammer.metrics/metrics-core/jars/metrics-core-2.2.0.jar
 JAR2=${HOME}/.ivy2/cache/org.apache.kafka/kafka_2.11/jars/kafka_2.11-0.8.2.1.jar
 JAR3=${HOME}/.ivy2/cache/org.apache.spark/spark-streaming-kafka_2.11/jars/spark-streaming-kafka_2.11-1.6.2.jar
+JAR4=${HOME}/.ivy2/cache/org.apache.kafka/kafka-clients/jars/kafka-clients-0.8.2.1.jar
 
 /usr/bin/kafkaproducer.py &
 
@@ -25,7 +26,7 @@ sleep 20
 
 echo "Executing Spark Streaming application ..."
 
-spark-submit                     \
-  --class="Streaming"            \
-  --jars ${JAR1},${JAR2},${JAR3} \
+spark-submit                             \
+  --class="Streaming"                    \
+  --jars ${JAR1},${JAR2},${JAR3},${JAR4} \
   /opt/SparkStreamingFromKafka/code/target/scala-2.11/streaming_2.11-1.0.jar
